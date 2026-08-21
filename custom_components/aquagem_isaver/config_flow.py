0c15ed9837c50bc252dd6898fd7df0ca997e677b
JP <contact@jptestart.com>
2026-08-21 06:52:32 +0200

    Expose polling and preset speeds in options

--- custom_components/aquagem_isaver/config_flow.py
+++ custom_components/aquagem_isaver/config_flow.py
@@ -9,11 +9,21 @@ from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT
 from homeassistant.core import callback
 
 from .const import (
+    CONF_DAY_SPEED,
+    CONF_ECO_SPEED,
+    CONF_MAX_SPEED,
+    CONF_NIGHT_SPEED,
     CONF_SCAN_INTERVAL,
+    DEFAULT_DAY_SPEED,
+    DEFAULT_ECO_SPEED,
+    DEFAULT_MAX_SPEED,
     DEFAULT_NAME,
+    DEFAULT_NIGHT_SPEED,
     DEFAULT_PORT,
     DEFAULT_SCAN_INTERVAL,
     DOMAIN,
+    MAX_SPEED,
+    MIN_SPEED,
 )
 from .protocol import AquagemClient, AquagemConnectionError
 
@@ -54,21 +64,39 @@ class AquagemConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
 
 class AquagemOptionsFlow(config_entries.OptionsFlow):
-    """Configure polling options."""
+    """Configure polling and Home Assistant speed profiles."""
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
+
+        options = self.config_entry.options
+        speed = vol.All(vol.Coerce(int), vol.Range(min=MIN_SPEED, max=MAX_SPEED))
+
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema(
                 {
                     vol.Required(
                         CONF_SCAN_INTERVAL,
-                        default=self.config_entry.options.get(
-                            CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
-                        ),
-                    ): vol.All(int, vol.Range(min=5, max=300))
+                        default=options.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL),
+                    ): vol.All(vol.Coerce(int), vol.Range(min=5, max=300)),
+                    vol.Required(
+                        CONF_NIGHT_SPEED,
+                        default=options.get(CONF_NIGHT_SPEED, DEFAULT_NIGHT_SPEED),
+                    ): speed,
+                    vol.Required(
+                        CONF_ECO_SPEED,
+                        default=options.get(CONF_ECO_SPEED, DEFAULT_ECO_SPEED),
+                    ): speed,
+                    vol.Required(
+                        CONF_DAY_SPEED,
+                        default=options.get(CONF_DAY_SPEED, DEFAULT_DAY_SPEED),
+                    ): speed,
+                    vol.Required(
+                        CONF_MAX_SPEED,
+                        default=options.get(CONF_MAX_SPEED, DEFAULT_MAX_SPEED),
+                    ): speed,
                 }
             ),
         )
