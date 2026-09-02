74a766d777a33fd85130e6467a550a002e844048
JP <dev@jptstar.com>
2026-09-02 18:03:01 +0200

    Add DM15 protocol constants

--- custom_components/aquagem_isaver/const.py
+++ custom_components/aquagem_isaver/const.py
@@ -1,4 +1,4 @@
-"""Constants for Aquagem iSaver."""
+"""Constants for Aquagem pump integrations."""
 
 DOMAIN = "aquagem_isaver"
 PLATFORMS = ["sensor", "number", "fan", "binary_sensor"]
@@ -10,12 +10,16 @@ CONF_NIGHT_SPEED = "night_speed"
 CONF_ECO_SPEED = "eco_speed"
 CONF_DAY_SPEED = "day_speed"
 CONF_MAX_PRESET_SPEED = "max_preset_speed"
+CONF_PROTOCOL = "protocol"
 
 DEFAULT_NAME = "iSaver Power 1100"
 DEFAULT_PORT = 502
 DEFAULT_SCAN_INTERVAL = 5
 
-# Physical protocol limits. Options may narrow this range, never extend it.
+PROTOCOL_ISAVER = "isaver_c3_d0"
+PROTOCOL_DM15 = "dm15_modbus"
+
+# iSaver Power 1100 proprietary C3/D0 profile.
 MIN_SPEED = 1200
 MAX_SPEED = 2900
 SPEED_STEP = 100
@@ -47,3 +51,15 @@ WRITE_FUNCTION = 0xD0
 READ_STATUS_BODY = bytes((DEVICE_ADDRESS, READ_FUNCTION, 0x07, 0xD1, 0x00, 0x02))
 WRITE_SPEED_PREFIX = bytes((DEVICE_ADDRESS, WRITE_FUNCTION, 0x0B, 0xB9))
 READ_STATUS_REPLY_LENGTH = 9
+
+# DM15 / INVERsilence standard Modbus RTU profile, validated on real hardware.
+DM15_READ_FUNCTION = 0x03
+DM15_WRITE_FUNCTION = 0x10
+DM15_STATUS_START = 2001
+DM15_STATUS_COUNT = 4
+DM15_COMMAND_REGISTER = 3001
+DM15_STATUS_REPLY_LENGTH = 13
+DM15_OFF_COMMAND = 0
+DM15_MIN_CAPACITY = 30
+DM15_MAX_CAPACITY = 100
+DM15_CAPACITY_STEP = 1
